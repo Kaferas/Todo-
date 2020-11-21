822247dd18cf8c3b4883175cd96b8b78c7a60a3d
Chris Martin <Chrisirak95@gmail.com>
2020-11-21 21:29:42 +0200

    Finalize Design

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("Pole Dear Ex")
+let insert=document.querySelector(".income-case")
+let expense=document.querySelector(".expense-case")
+let validation=document.querySelector(".validation")
+let sign
+
+function createCase(inserted)
+{
+    let element= document.createElement("div")
+    let label=document.createElement("span")
+    let price=document.createElement("span")
+    let pourcent=document.createElement("span")
+
+    element.style.borderTop="1px solid black"
+    element.style.borderBottom="1px solid black"
+    element.style.marginTop="30px"
+    element.style.height="35px"
+    element.style.width="35vw"
+    element.style.marginLeft="30px"
+
+    label.classList.add("label")
+    price.classList.add("price")
+    pourcent.classList.add("pourcent")
+
+    element.appendChild(label)
+    element.appendChild(price)
+    element.appendChild(pourcent)
+    inserted.appendChild(element)
+}
+
+    validation.addEventListener("click",function(){
+        sign=document.getElementById("sign").value
+        alert(sign)
+    })
+
